6ce568ef7552664e7cad1ab4be012547c4248f5a
Kian Naimi-Roy <121135813+KianNaimiRoy@users.noreply.github.com>
2023-06-27 09:45:08 -0700

    Make image a hyperlink to URL

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ fetch (`https://api.unsplash.com/photos/random?client_id=${UNSPLASH_ACCESS_KEY}`
 .then((res) => res.json()) // Converts response to JSON
 .then((data) => { 
   const imageElement = document.getElementById('randomImage');
+  const linkElement = document.getElementById('imageLink');
   imageElement.src = data.urls.regular; // Sets src element to random image from Unsplash
+  linkElement.href = data.links.html; // Sets the href of the link to the image URL
 })
 .catch((error) => {
   console.log('Error:', error);
